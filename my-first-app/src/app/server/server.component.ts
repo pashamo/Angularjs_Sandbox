614bcc6a7479b420d4a5cf847fcb9def179d1581
Mohammed Pasha <mk.pasha@hotmail.com>
2021-06-25 18:32:10 -0400

    fix: Using inline templating for ServerComponent

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    template: `<p>The ServerComponent</p>`
 })
 
 export class ServerComponent {
