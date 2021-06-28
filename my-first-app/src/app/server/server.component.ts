6fe42db86904d7b932c3173eb587362087301adb
Mohammed Pasha <mk.pasha@hotmail.com>
2021-06-28 16:24:00 -0400

    feat: Attribute Directive

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -2,12 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
-    template: `<p>The ServerComponent - Server with id {{ serverID }} is {{ getServerStatus() }}</p>`
+    template: `
+    <p [ngStyle]="{ backgroundColor: getColour() }">The ServerComponent - Server with id {{ serverID }} is {{ getServerStatus() }}</p>
+    `
 })
 
 export class ServerComponent {
     serverID: number = 10;
     serverStatus: string = 'offline';
 
-    getServerStatus = () => this.serverStatus;
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
+    getServerStatus() {
+        return this.serverStatus;
+    }
+
+    getColour() {
+        return this.serverStatus === 'online' ? 'green':'firebrick';
+    }
 }
