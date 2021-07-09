41b6c04c739bc98feeaed65d05ecb967dee6a8ec
Mohammed Pasha <mk.pasha@hotmail.com>
2021-07-08 23:54:51 -0400

    feat: producing all in one component

--- my-second-app/src/app/app.component.ts
+++ my-second-app/src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-second-app';
+  newServerName: string;
+  newServerContent: string;
+  serverElements: any[] = [];
+
+  onAddServer() {
+    this.serverElements.push({
+        "type": "server", 
+        "sname": this.newServerName,
+        "scontent": this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      "type": "blueprint",
+      "sname": this.newServerName,
+      "scontent": this.newServerContent
+    });
+  }
 }
