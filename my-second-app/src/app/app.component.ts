5399a03904ec9be374145743055dbda33a3dbaa9
Mohammed Pasha <mk.pasha@hotmail.com>
2021-07-09 00:08:14 -0400

    feat: split app into multiple components

--- my-second-app/src/app/app.component.ts
+++ my-second-app/src/app/app.component.ts
@@ -6,23 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  newServerName: string;
-  newServerContent: string;
-  serverElements: any[] = [];
-
-  onAddServer() {
-    this.serverElements.push({
-        "type": "server", 
-        "sname": this.newServerName,
-        "scontent": this.newServerContent
-    });
-  }
-
-  onAddBlueprint() {
-    this.serverElements.push({
-      "type": "blueprint",
-      "sname": this.newServerName,
-      "scontent": this.newServerContent
-    });
-  }
+  
 }
---
+++ my-second-app/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'app-cockpit',
+    templateUrl: './cockpit.component.html',
+    styleUrls: ['./cockpit.component.css']
+})
+
+export class CockpitComponent {
+    newServerName: string;
+    newServerContent: string;
+    serverElements: any[] = [];
+
+    constructor() {}
+
+    ngOnInit() {}
+
+    onAddServer() {
+        this.serverElements.push({
+            "type": "server", 
+            "sname": this.newServerName,
+            "scontent": this.newServerContent
+        });
+    }
+
+    onAddBlueprint() {
+        this.serverElements.push({
+            "type": "blueprint",
+            "sname": this.newServerName,
+            "scontent": this.newServerContent
+        });
+    }
+}
